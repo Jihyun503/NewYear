b4a8d5f53df540f80e003a32c9b3f30c346a41b1
tofujoah <tofu@consalad.io>
2023-01-17 16:20:00 +0900

    record crud 기능 수정

--- record/views.py
+++ record/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from .permissions import IsOwnerOrReadOnly
 from .models import Record
 from goal.models import Goal
 from .serializers import RecordSerializer
@@ -12,7 +11,12 @@ from django.db.models import Max
 class RecordViewSet(viewsets.ModelViewSet):
     serializer_class = RecordSerializer
     queryset = Record.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
     
+    def get_queryset(self):
+        queryset = self.queryset
+        return queryset
+
     def list(self, request):
         records = self.queryset.filter(user=request.user)
         serializer = RecordSerializer(records, many=True)
@@ -20,11 +24,12 @@ class RecordViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
      
     def create(self, request):
+        goal = get_object_or_404(Goal.objects.filter(user=request.user, bno=request.data.get("goal")))
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(user=self.request.user)
 
-        goal = Goal.objects.get(bno=request.data.get("goal"))
         if goal.percent < int(request.data.get("percent")):  # 퍼센트가 줄지 않고 상승만 한다는 전제 하에
             goal.percent = request.data.get("percent")
 
@@ -33,7 +38,9 @@ class RecordViewSet(viewsets.ModelViewSet):
         return Response(request.data)
     
     def update(self, request, pk):
-        instance = get_object_or_404(self.queryset, id=pk)
+        #instance = get_object_or_404(self.queryset, id=pk)
+        instance = get_object_or_404(self.queryset.filter(user=request.user, id=pk))
+        
         serializer = self.get_serializer(instance, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -46,26 +53,23 @@ class RecordViewSet(viewsets.ModelViewSet):
 
         return Response(request.data)
 
-    def delete(self, request, pk):
-        print("들어왓다고;;")
+    def destroy(self, request, pk):
         instance = get_object_or_404(self.queryset, id=pk)
-        serializer = self.get_serializer(instance, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        print("들어옴")
-        try:
-            qs = Record.objects.all(goal=request.data.get("goal"))
-        except Exception:
-            goal = Goal.objects.get(bno=request.data.get("goal"))
+        stdgoal = instance.goal
+        instance.delete()
+
+        goal = Goal.objects.get(bno=stdgoal.bno)
+        record = Record.objects.filter(goal=stdgoal)
+
+        if record.exists() is False:
             goal.percent = 0
             goal.save()
-        
-        obj = Record.objects.aggregate(percent_max=Max('percent'))
-        print(obj)
 
-        goal = Goal.objects.get(bno=request.data.get("goal"))
+            return Response(request.data)
+
+        obj = record.aggregate(percent_max=Max('percent'))
+
         goal.percent = obj['percent_max']
         goal.save()
 
         return Response(request.data)
-
